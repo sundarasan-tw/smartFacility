eb70a5733d5d54f45f141cd135a0ca4305a8137d
ranjithkumar-tw <ranjithkumar.c@thoughtworks.com>
2025-03-28 11:49:53 +0530

    partial test setup
    test setup for bff with make file targets and scirpt file changes
    
    fixed make test in dockerfile and refactored gitignore
    
    adding changes for test,test-report and poetry-add commands

---
+++ scripts/run-make.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "Building Web bff Docker image for development..."
+docker build --progress=plain -t web-bff-dev -f ./web-bff/Dockerfile.dev ./web-bff
+
+echo "Running Web bff Docker container for test and coverage reports..."
+docker run -p 8000:8000 --name web-bff-dev-container \
+	-v $(PWD)/web-bff/app:/src/app \
+	-v $(PWD)/web-bff/tests:/src/tests \
+	-v $(PWD)/web-bff/test-reports:/src/test-reports \
+	-v $(PWD)/web-bff/coverage-reports:/src/coverage-reports \
+	--rm \
+	web-bff-dev \
+  "$@"
