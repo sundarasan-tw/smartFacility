104528e32dfec9502a19c4e9c4eae75c1f6f6ade
sundarasan-tw <sundarasan.n@thoughtworks.com>
2025-04-01 21:48:52 +0530

    Add local and CI lint & format setup

--- web-bff/app/presentation/health_routes.py
+++ web-bff/app/presentation/health_routes.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
-from app.application.device_service import DeviceService
-from app.domain.models.device_model import DeviceDTO
-
 health_router = APIRouter()
+
 
 @health_router.get("/health", response_model=dict)
 async def health_check():
