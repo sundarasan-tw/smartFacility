3e3cb897328034ae567d7d271e9ec44ab1e546b5
sundarasan-tw <sundarasan.n@thoughtworks.com>
2025-04-01 21:48:52 +0530

    Add local and CI lint & format setup

--- scripts/run-make.sh
+++ scripts/run-make.sh
@@ -2,7 +2,7 @@
 echo "Building Web bff Docker image for development..."
 docker build --progress=plain -t web-bff-dev -f ./web-bff/Dockerfile.dev ./web-bff
 
-echo "Running Web bff Docker container for test and coverage reports..."
+echo "Running Web BFF cocker image for development..."
 docker run -p 8000:8000 --name web-bff-dev-container \
 	-v $(PWD)/web-bff/app:/src/app \
 	-v $(PWD)/web-bff/tests:/src/tests \
