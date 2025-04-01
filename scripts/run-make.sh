97eda2ae2213f1967d1aeedcef9ba5c35582a7d8
sundarasan-tw <sundarasan.n@thoughtworks.com>
2025-04-01 14:58:09 +0530

    Update CI to run and upload test reports

--- scripts/run-make.sh
+++ scripts/run-make.sh
@@ -6,8 +6,7 @@ echo "Running Web bff Docker container for test and coverage reports..."
 docker run -p 8000:8000 --name web-bff-dev-container \
 	-v $(PWD)/web-bff/app:/src/app \
 	-v $(PWD)/web-bff/tests:/src/tests \
-	-v $(PWD)/web-bff/test-reports:/src/test-reports \
-	-v $(PWD)/web-bff/coverage-reports:/src/coverage-reports \
+	-v $(PWD)/web-bff/reports:/src/reports \
 	--rm \
 	web-bff-dev \
   "$@"
